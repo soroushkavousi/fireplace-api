99dcdea773bc27d15ed37bca9840d9aab122987a
Soroush Kavousi <soroushkavousi.me@gmail.com>
2022-03-06 09:05:51 +0330

    fix: Fix swagger server address bug

--- Api/Tools/Swagger/DocumentFilters/CustomModelDocumentFilter.cs
+++ Api/Tools/Swagger/DocumentFilters/CustomModelDocumentFilter.cs
@@ -2,31 +2,20 @@
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
-using System.Collections.Generic;
 
 namespace FireplaceApi.Api.Tools
 {
-    //Usage : "options.DocumentFilter<CustomModelDocumentFilter<Error>>();"
     public class CustomModelDocumentFilter<T> : IDocumentFilter where T : class
     {
         private readonly ILogger<CustomModelDocumentFilter<T>> _logger;
-        private readonly string _swaggerDocHost;
-        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public CustomModelDocumentFilter(ILogger<CustomModelDocumentFilter<T>> logger, IHttpContextAccessor httpContextAccessor)
+        public CustomModelDocumentFilter(ILogger<CustomModelDocumentFilter<T>> logger)
         {
             _logger = logger;
-            var scheme = httpContextAccessor?.HttpContext?.Request?.Scheme;
-            var host = httpContextAccessor?.HttpContext?.Request?.Host;
-            var pathBase = httpContextAccessor?.HttpContext?.Request?.PathBase;
-            _swaggerDocHost = $"{scheme}://{host}{pathBase}";
-            _httpContextAccessor = httpContextAccessor;
-            logger.LogInformation($"_swaggerDocHost : {_swaggerDocHost}");
         }
 
         public void Apply(OpenApiDocument openapiDoc, DocumentFilterContext context)
         {
-            openapiDoc.Servers = new List<OpenApiServer>() { new OpenApiServer() { Url = _swaggerDocHost } };
             context.SchemaGenerator.GenerateSchema(typeof(T), context.SchemaRepository);
         }
     }
