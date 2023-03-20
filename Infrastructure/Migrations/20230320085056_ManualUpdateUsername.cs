2930d506e5353ef4d610fac7a3c611d0e48381aa
soroushkavousi <soroushkavousi.me@gmail.com>
2023-03-20 12:44:31 +0330

    fix: Add back UpdateUsername stored procedure

---
+++ Infrastructure/Migrations/20230320085056_ManualUpdateUsername.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FireplaceApi.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ManualUpdateUsername : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"
+                CREATE OR REPLACE PROCEDURE public.""UpdateUsername""(
+	                ""UserId"" numeric,
+	                ""NewUsername"" text)
+                LANGUAGE 'sql'
+                AS $BODY$
+                UPDATE public.""UserEntities""
+                SET ""Username"" = ""NewUsername""
+                WHERE ""Id"" = ""UserId"";
+                $BODY$;";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"
+                DROP PROCEDURE public.""UpdateUsername"";
+                ";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
